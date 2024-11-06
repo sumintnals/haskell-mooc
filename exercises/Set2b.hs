f72ea8fd59d44769433ed9bb16ce3504030ae22a
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-11-07 01:19:09 +0900

    feat: Set2b EX1

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -16,7 +16,10 @@ import Data.List
 -- Hint! pattern matching is your friend.
 
 binomial :: Integer -> Integer -> Integer
-binomial = todo
+binomial n k
+  | k == 0    = 1                         
+  | n == 0    = 0                         
+  | otherwise = binomial (n - 1) k + binomial (n - 1) (k - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the odd factorial function. Odd factorial is like
